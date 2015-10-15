e5ceeaf1ca7feaae1fbd74b57c33e341a42f1aa5
Azam khan <azambhrgn@gmail.com>
2015-10-15 18:54:37 +0530

    New file from Company PC

---
+++ src/com/az/linkedlist/List1.java
@@ -0,0 +1,44 @@
+package com.az.linkedlist;
+
+
+
+class Node {
+	int num;
+	Node next;
+}
+
+class ListOpr {
+	Node head;
+	public void insertList()
+	{
+		
+		
+	}
+	
+	public void display()
+	{
+		
+		
+	}
+	
+	public void setNode(Node head)
+	{
+		this.head=head;
+	}
+	public Node getHead()
+	{
+		return head;
+	}
+	
+}
+
+
+
+public class List1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
